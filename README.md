5e5335c92c22116f178c05a227c0faa4ee73646c
Yuichiro Nakada <berry@berry-lab.net>
2020-04-05 15:24:02 +0900

    support svg

--- README.md
+++ README.md
@@ -16,16 +16,19 @@ Usage: ./img2ras [options] file
 
 Options:
 -h                 Print this message
--o <output name>   Output file name
--c                 Reduce color [default:32]
+-o <output name>   Output file name [default: img2ras.eps]
+-svg               Output file type [default: eps]
+-c <num>           Reduce color [default: 32]
 -b <scale>         Blur image
 
 $ ./img2ras girl-1118419_1280.jpg -c 2 -o girl-1118419.eps
-$ ./img2ras sparkler-677774_1920.jpg -c 32 -x -o sparkler-677774.eps
-$ ./img2ras publicdomainq-0041064ikt.jpg -c 8 -a -b 4 -o publicdomainq-0041064ikt.eps
+$ ./img2ras publicdomainq-0041064ikt.jpg -c 8 -a -b 10 -o publicdomainq-0041064ikt.eps
 $ ./img2ras publicdomainq-0017653mro.jpg -c 5 -a -o publicdomainq-0017653mro.eps
-$ ./img2ras 1098751.jpg -o 1098751.eps -c 32 -a -b 4
+$ ./img2ras hairdress-4912246.jpg -o hairdress-4912246.svg -svg -a
+
+$ ./img2ras 1098751.jpg -o 1098751.eps -c 48 -a -cx -b 14
 $ ./img2ras night-4926430_1920.jpg -c 16 -o night-4926430.eps
+$ ./img2ras sparkler-677774_1920.jpg -c 32 -x -o sparkler-677774.eps
 ```
 
 ## Example
@@ -36,12 +39,6 @@ Original image (https://pixabay.com/ja/illustrations/%E5%A5%B3%E3%81%AE%E5%AD%90
 
 img2ras output
 ![Output](girl-1118419.svg)
-
-Original image
-![Original](sparkler-677774_1920.jpg)
-
-img2ras output
-![Output](sparkler-677774.svg)
 
 Original image
 ![Original](publicdomainq-0041064ikt.jpg)
@@ -60,4 +57,10 @@ Original image
 
 img2ras output
 ![Output](night-4926430.svg)
+
+Original image
+![Original](sparkler-677774_1920.jpg)
+
+img2ras output
+![Output](sparkler-677774.svg)
 
--- img2ras.c
+++ img2ras.c
@@ -11,7 +11,7 @@
 #include "imgp.h"
 
 #include "potracelib.h"
-int img2ras(FILE *fp, uint8_t *s, int w, int h, int r, int g, int b)
+int img2ras(FILE *fp, uint8_t *s, int w, int h, int r, int g, int b, int flag)
 {
 	// create a bitmap
 	potrace_bitmap_t *bm = bm_new(w, h);
@@ -45,27 +45,31 @@ int img2ras(FILE *fp, uint8_t *s, int w, int h, int r, int g, int b)
 	bm_free(bm);
 
 	// output vector data, e.g. as a rudimentary EPS file
-//	printf("%%!PS-Adobe-3.0 EPSF-3.0\n");
-//	printf("%%%%BoundingBox: 0 0 %d %d\n", w, h);
-	fprintf(fp, "gsave\n");
+	//printf("%%!PS-Adobe-3.0 EPSF-3.0\n");
+	//printf("%%%%BoundingBox: 0 0 %d %d\n", w, h);
+	if (!(flag&32)) fprintf(fp, "gsave\n");
 	potrace_path_t *p = st->plist;
 	potrace_dpoint_t (*c)[3];
 	while (p != NULL) {
 		int n = p->curve.n;
 		int *tag = p->curve.tag;
 		c = p->curve.c;
-		fprintf(fp, "%f %f moveto\n", c[n-1][2].x, c[n-1][2].y);
+		if (!(flag&32)) fprintf(fp, "%f %f moveto\n", c[n-1][2].x, c[n-1][2].y);
+		if (flag&32) fprintf(fp, "<path d=\"M%f,%f", c[n-1][2].x, h-c[n-1][2].y);
 		for (int i=0; i<n; i++) {
 			switch (tag[i]) {
 			case POTRACE_CORNER:
-				fprintf(fp, "%f %f lineto\n", c[i][1].x, c[i][1].y);
-				fprintf(fp, "%f %f lineto\n", c[i][2].x, c[i][2].y);
+				if (!(flag&32)) fprintf(fp, "%f %f lineto\n", c[i][1].x, c[i][1].y);
+				if (!(flag&32)) fprintf(fp, "%f %f lineto\n", c[i][2].x, c[i][2].y);
+				if (flag&32) fprintf(fp, " L%f,%f L%f,%f", c[i][1].x, h-c[i][1].y, c[i][2].x, h-c[i][2].y);
 				break;
 			case POTRACE_CURVETO:
-				fprintf(fp, "%f %f %f %f %f %f curveto\n",
+				if (!(flag&32)) fprintf(fp, "%f %f %f %f %f %f curveto\n",
 				       c[i][0].x, c[i][0].y,
 				       c[i][1].x, c[i][1].y,
 				       c[i][2].x, c[i][2].y);
+				if (flag&32) fprintf(fp, " C%f,%f %f,%f %f,%f",
+					c[i][0].x, h-c[i][0].y, c[i][1].x, h-c[i][1].y, c[i][2].x, h-c[i][2].y);
 				break;
 			}
 		}
@@ -73,11 +77,18 @@ int img2ras(FILE *fp, uint8_t *s, int w, int h, int r, int g, int b)
 		   children, fill. */
 		if (p->next == NULL || p->next->sign == '+') {
 			//printf("0 setgray fill\n");
-			fprintf(fp, "%f %f %f setrgbcolor fill\n", r/255.0, g/255.0, b/255.0);
+			if (!(flag&32)) fprintf(fp, "%f %f %f setrgbcolor fill\n", r/255.0, g/255.0, b/255.0);
+//			if (flag&32) fprintf(fp, "\" stroke=\"#%02x%02x%02x\"/>", r, g, b);
+//		} else {
+//			if (flag&32) fprintf(fp, "\" fill=\"#%02x%02x%02x\"/>", r, g, b);
 		}
 		p = p->next;
+		//if (flag&32) fprintf(fp, "\" fill=\"#%02x%02x%02x\" stroke=\"#%02x%02x%02x", r, g, b, r, g, b);
+		//if (flag&32) fprintf(fp, "\"/>\n");
+//		if (flag&32) fprintf(fp, "\" stroke=\"#%02x%02x%02x\"/>", r, g, b);
+		if (flag&32) fprintf(fp, "\" fill=\"#%02x%02x%02x\"/>", r, g, b);
 	}
-	fprintf(fp, "grestore\n");
+	if (!(flag&32)) fprintf(fp, "grestore\n");
 	//printf("%%EOF\n");
 
 	return 0;
@@ -115,8 +126,9 @@ void color_quant(unsigned char *im, int w, int h, int n_colors, char *name, int 
 	// output
 	if (flag&1) stbi_write_jpg("posterized.jpg", w, h, 3, im, 0);
 	FILE *fp = fopen(name, "w");
-	fprintf(fp, "%%!PS-Adobe-3.0 EPSF-3.0\n");
-	fprintf(fp, "%%%%BoundingBox: 0 0 %d %d\n", w, h);
+	if (!(flag&32)) fprintf(fp, "%%!PS-Adobe-3.0 EPSF-3.0\n");
+	if (!(flag&32)) fprintf(fp, "%%%%BoundingBox: 0 0 %d %d\n", w, h);
+	if (flag&32) fprintf(fp, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%dpx\" height=\"%dpx\" viewBox=\"0 0 %d %d\">\n", w, h, w, h);
 	uint8_t *img = malloc(w * h *3*2);
 	for (i=1; i < heap.n; i++) {
 		memset(img, 0, w * h *3);
@@ -142,13 +154,14 @@ void color_quant(unsigned char *im, int w, int h, int n_colors, char *name, int 
 		}
 		if (flag&2) { // dilate
 			imgp_dilate(img, w, h, 3, img+w * h *3);
-			img2ras(fp, img+w * h *3, w, h, got->r, got->g, got->b);
+			img2ras(fp, img+w * h *3, w, h, got->r, got->g, got->b, flag);
 		} else {
-			img2ras(fp, img, w, h, got->r, got->g, got->b);
+			img2ras(fp, img, w, h, got->r, got->g, got->b, flag);
 		}
 	}
 	free(img);
-	fprintf(fp, "%%EOF\n");
+	if (!(flag&32)) fprintf(fp, "%%EOF\n");
+	if (flag&32) fprintf(fp, "</svg>\n");
 	fclose(fp);
 
 	node_free();
@@ -189,8 +202,9 @@ void usage(FILE* fp, char** argv)
 		"Usage: %s [options] file\n\n"
 		"Options:\n"
 		"-h                 Print this message\n"
-		"-o <output name>   Output file name\n"
-		"-c                 Reduce color [default:32]\n"
+		"-o <output name>   Output file name [default: img2ras.eps]\n"
+		"-svg               Output file type [default: eps]\n"
+		"-c <num>           Reduce color [default: 32]\n"
 		"-b <scale>         Blur image\n"
 		"\n",
 		argv[0]);
@@ -199,7 +213,7 @@ void usage(FILE* fp, char** argv)
 int main(int argc, char* argv[])
 {
 	char *name = argv[1];
-	char *outfile = "output.eps";
+	char *outfile = "img2ras.eps";
 	int color = 32;
 	int flag = 0;
 	float scale = 2;
@@ -214,14 +228,18 @@ int main(int argc, char* argv[])
 		} else if (!strcmp(argv[i], "-c")) {
 			color = atoi(argv[++i]);
 		} else if (!strcmp(argv[i], "-d")) {
-			flag = 1; // debug
+			flag |= 1; // debug
 		} else if (!strcmp(argv[i], "-x")) {
-			flag = 2; // dilate
+			flag |= 2; // dilate
 		} else if (!strcmp(argv[i], "-a")) {
-			flag = 4; // alpha
+			flag |= 4; // alpha
 		} else if (!strcmp(argv[i], "-b")) {
-			flag = 8; // blur
+			flag |= 8; // blur
 			scale = atof(argv[++i]);
+		} else if (!strcmp(argv[i], "-cx")) {
+			flag |= 16;
+		} else if (!strcmp(argv[i], "-svg")) {
+			flag |= 32;
 		} else if (!strcmp(argv[i], "-h")) {
 			usage(stderr, argv);
 			return 0;
@@ -250,38 +268,40 @@ int main(int argc, char* argv[])
 	_imgp_dilate(posterized, w, h, 3, posterized+w*h*3);
 	stbi_write_jpg("original_dilated.jpg", w, h, 3, posterized+w*h*3, 0);
 	free(posterized);*/
-/*	uint8_t *posterized = malloc(w*h*3*2);
-	imgp_filter(posterized, pixels, w, h, magic_kernel, 4, 1, 0);
-	stbi_write_jpg("magic.jpg", w, h, 3, posterized, 0);
-	color_quant(&(image_t) {w, h, posterized}, 64);
-	stbi_write_bmp("original_m64.bmp", w, h, 3, posterized);
-	free(posterized);*/
 	if (flag&8) { // blur
 		int sx = w*scale;	// 2000
 		int sy = h*scale;	// 2800
 		uint8_t *posterized = malloc(sx*sy *3 *2);
 		stbir_resize_uint8_srgb(pixels, w, h, 0, posterized+sx*sy*3, sx, sy, 0, 3, -1, 0);
-		imgp_filter(posterized, posterized+sx*sy*3, sx, sy, magic_kernel, 4, 1, 0);
+		imgp_filter(posterized+sx*sy*3, sx, sy, posterized, magic_kernel, 4, 1, 0);
 		stbir_resize_uint8_srgb(posterized, sx, sy, 0, /*posterized+sx*sy*3*/pixels, w, h, 0, 3, -1, 0);
 		if (flag&1) stbi_write_jpg("magic.jpg", w, h, 3, /*posterized+sx*sy*3*/pixels, 0);
 		free(posterized);
 	}
+	if (flag&16) {
+		//imgp_cq24to15(pixels, w, h, 3, pixels, 1);
+		uint8_t *p = pixels;
+		for (int n=0; n<w*h*3; n++) {
+			*p = *p & 0b11110000;
+			p++;
+		}
+	}
 
 	uint8_t *gray = malloc(w*h*4);
-	uint8_t *dilated = gray+w*h;
-	uint8_t *diff = gray+w*h*2;
-	uint8_t *contour = gray+w*h*3;
+//	uint8_t *dilated = gray+w*h;
+//	uint8_t *diff = gray+w*h*2;
+//	uint8_t *contour = gray+w*h*3;
 	imgp_gray(pixels, w, h, w, gray, w);
 	color_quant(pixels, w, h, color, outfile, flag);
-//	stbi_write_bmp("original64.bmp", w, h, 3, pixels);
-//	stbi_write_bmp("gray.bmp", w, h, 1, gray);
-	imgp_dilate(gray, w, h, 1, dilated);
-//	stbi_write_jpg("dilated.jpg", w, h, 1, dilated, 0);
-	imgp_absdiff(gray, dilated, w, h, diff);
-//	stbi_write_jpg("diff.jpg", w, h, 1, diff, 0);
-	imgp_reverse(diff, w, h, contour);
-//	stbi_write_jpg("contour.jpg", w, h, 1, contour, 0);
-//	stbi_write_bmp("contour.bmp", w, h, 1, contour);
+	//stbi_write_bmp("original64.bmp", w, h, 3, pixels);
+	//stbi_write_bmp("gray.bmp", w, h, 1, gray);
+//	imgp_dilate(gray, w, h, 1, dilated);
+	//stbi_write_jpg("dilated.jpg", w, h, 1, dilated, 0);
+//	imgp_absdiff(gray, dilated, w, h, diff);
+	//stbi_write_jpg("diff.jpg", w, h, 1, diff, 0);
+//	imgp_reverse(diff, w, h, contour);
+	//stbi_write_jpg("contour.jpg", w, h, 1, contour, 0);
+	//stbi_write_bmp("contour.bmp", w, h, 1, contour);
 	free(gray);
 	stbi_image_free(pixels);
 }
